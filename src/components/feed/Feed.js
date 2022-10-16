faf1f3e0b98dba19e93a0126ce5999470bf40c65
dboatenx <dboaten99@gmail.com>
2022-10-15 22:46:49 -0400

    update 10.46

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -4,14 +4,24 @@ import "./feed.css"
 export default function Feed() {
   return (
     <div className="feed">
-        <div className="title">
-            <h1> Top Health News </h1>
-        </div>
-        <span className="hr">
-        </span>
+
+      <div className="title">
+          <h1> Top Health News </h1>
+      </div>
+        
+      <div className="hrline"> </div>
+
+      <article class="design">
+          <div class="text1">
+              <h3> Design</h3>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis officiis aliquid adipisci veritatis, ad numquam et! In assumenda dolore quam, corrupti, dignissimos, soluta est praesentium vel eligendi quibusdam beatae at!</p>
+          </div>
+          <img src="/assets/newsfeedthumbnail/thumb1.jpg" alt=""></img>
+        </article>
 
     </div>
     
+
         
   )
 }
