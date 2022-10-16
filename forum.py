92b5cc073757ac9db5e0150076f9aec62895d70c
zhezhenwang <36493346+zhezhenwang@users.noreply.github.com>
2022-10-15 22:29:07 -0400

    forum.py

---
+++ forum.py
@@ -0,0 +1,125 @@
+
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import User, Disease, Post, Comment
+from .utils import update_views
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+def home(request):
+    forums = Disease.objects.all()
+    num_posts = Post.objects.all().count()
+    num_users = User.objects.all().count()
+    num_categories = forums.count()
+    try:
+        last_post = Post.objects.latest("date")
+    except:
+        last_post = []
+
+    context = {
+        "forums":forums,
+        "num_posts":num_posts,
+        "num_users":num_users,
+        "num_categories":num_categories,
+        "last_post":last_post,
+        "title": "OZONE forum app"
+    }
+    return render(request, "forums.html", context)
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.user.is_authenticated:
+        author = User.objects.get(user=request.user)
+    
+    if "comment-form" in request.POST:
+        comment = request.POST.get("comment")
+        new_comment, created = Comment.objects.get_or_create(user=author, content=comment)
+        post.comments.add(new_comment.id)
+
+    context = {
+        "post":post,
+        "title": "OZONE: "+post.title,
+    }
+    update_views(request, post)
+
+    return render(request, "detail.html", context)
+
+def posts(request, slug):
+    category = get_object_or_404(Disease, slug=slug)
+    posts = Post.objects.filter(approved=True, categories=category)
+    paginator = Paginator(posts, 5)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages) 
+
+    context = {
+        "posts":posts,
+        "forum": category,
+        "title": "OZONE: Posts"
+    }
+
+    return render(request, "posts.html", context)
+
+
+@login_required
+def create_post(request):
+    context = {}
+    form = PostForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            print("\n\n its valid")
+            author = User.objects.get(user=request.user)
+            new_post = form.save(commit=False)
+            new_post.user = author
+            new_post.save()
+            form.save_m2m()
+            return redirect("home")
+    context.update({
+        "form": form,
+        "title": "OZONE: Create New Post"
+    })
+    return render(request, "create_post.html", context)
+
+def latest_posts(request):
+    posts = Post.objects.all().filter(approved=True)[:10]
+    context = {
+        "posts":posts,
+        "title": "OZONE: Latest 10 Posts"
+    }
+
+    return render(request, "latest-posts.html", context)
+
+def search_result(request):
+
+    return render(request, "search.html")
+
+
+def delete_post(request,id):
+    post = Post.objects.get(id=id)
+    if request.user.is_authenticated and request.user == post.author:
+        Post.objects.filter(id=post_id).delete()
+        return redirect('posts:mypost')
+
+def edit_post(request):
+    post = Post.objects.get(id=id)
+    if request.user.is_authenticated and request.user == post.author:
+        if request.method == 'POST':
+        form = PostCreateForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts:mypost')
+
+        else:
+            form = PostCreateForm(instance=post)
+
+        args = {}
+        args.update(csrf(request))
+        args['form'] = form
+
+    return render_to_response('posts/post_edit.html', args)
+
