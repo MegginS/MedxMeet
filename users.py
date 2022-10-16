eacf19c7eff15d38cd686747d004e49dda547dba
zhezhenwang <36493346+zhezhenwang@users.noreply.github.com>
2022-10-16 11:23:19 -0400

    Update and rename views.py to users.py

--- users.py
+++ users.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from register.forms import UpdateForm
+from .forms import UpdateForm
 from django.contrib.auth import logout as lt
 from django.contrib import messages
 from django.contrib.auth.models import User
