6eac98bf132f59582d246a87b613a1c3d9b885d8
dboatenx <dboaten99@gmail.com>
2022-10-15 20:06:26 -0400

    Update
    Added hr, div and aligned the contents on the page

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -4,11 +4,30 @@ import "./feed.css"
 export default function Feed() {
   return (
     <div className="feed">
-        <div className="title">
-            <h1> Top Health News </h1>
+      <div className="title">
+          <h1> Top Health News </h1>
+      </div>
+    
+      
+      <div className="hr"></div>
+
+      <div className="main">
+
+        <div className="introtext">
+        <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea aspernatur rerum vel autem quidem numquam debitis excepturi unde? Obcaecati iusto adipisci repudiandae suscipit iure magnam minima odit facilis facere optio?
+        </p>
         </div>
-        <span className="hr">
-        </span>
+        
+
+        <article class="design">
+                <div class="text1">
+                    <h3> Design</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis officiis aliquid adipisci veritatis, ad numquam et! In assumenda dolore quam, corrupti, dignissimos, soluta est praesentium vel eligendi quibusdam beatae at!</p>
+                </div>
+                <img src="" alt="" />
+            </article>
+      </div>
 
     </div>
     
